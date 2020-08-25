45c8a4160dce0d8202c043393c257a9f6628b840
tobeefansis <tftsis859@gmail.com>
2020-08-25 18:32:30 +0300

    Create ScenesLoader.cs

---
+++ Assets/Scripts/ScenesLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScenesLoader : MonoBehaviour
+{
+
+    public void Load(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+    public void Load(int s)
+    {
+        SceneManager.LoadScene(s);
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
