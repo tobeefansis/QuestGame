cf82b53f25e8106d564b8d33ef6f3f8a316ca743
tobeefansis <tftsis859@gmail.com>
2020-08-25 18:32:08 +0300

    Create SavedObject.cs

---
+++ Assets/Scripts/SavedObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+public abstract class SavedObject : MonoBehaviour
+{
+    public string path;
+    public void Save()
+    {
+        PlayerPrefs.SetString(path, GetValue());
+    }
+    private void OnEnable()
+    {
+        Load();
+    }
+    private void OnDisable()
+    {
+        Save();
+    }
+    private void OnApplicationQuit()
+    {
+        Save();
+    }
+    public string GetValue()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public void SetValue(string value)
+    {
+        JsonUtility.FromJsonOverwrite(value, this);
+    }
+    [InitializeOnEnterPlayMode]
+    public void Load()
+    {
+        var str = PlayerPrefs.GetString(path);
+        SetValue(str);
+    }
+    private void Awake()
+    {
+        Load();
+    }
+
+}
