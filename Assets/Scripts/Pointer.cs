68bf616c68f2b6cfb292e0d384c48f61b0afb10f
tobeefansis <tftsis859@gmail.com>
2020-08-26 18:54:52 +0300

    all meta

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pointer : MonoBehaviour, IPause
+{
+    public static Pointer Instance;
+
+    [SerializeField] Transform PointerUI;
+    [SerializeField] LayerMask layerMask;
+    Camera cam;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+        cam = Camera.main;
+    }
+    public Transform position;
+
+    private void LateUpdate()
+    {
+
+        if (Physics.Raycast(cam.ScreenPointToRay(PointerUI.position), out RaycastHit hit, layerMask))
+        {
+            if (hit.transform)
+            {
+
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Resume()
+    {
+        throw new System.NotImplementedException();
+    }
+}
