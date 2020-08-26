d6034c72b5d7c1931bf01e6ea4a516996f68c334
tobeefansis <tftsis859@gmail.com>
2020-08-26 18:14:36 +0300

    Create PauseManager

---
+++ Assets/Scripts/IPause.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IPause
+{
+    void Pause();
+    void Resume();
+}
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PauseManager : MonoBehaviour
+{
+    public static PauseManager Instance;
+    [SerializeField] List<IPause> pauses = new List<IPause>();
+
+    public List<IPause> Pauses { get => pauses; set => pauses = value; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            var objcets = FindObjectsOfType<Transform>();
+            foreach (var item in objcets)
+            {
+                var obj = item.GetComponent<IPause>();
+                if (obj != null)
+                {
+                    pauses.Add(obj);
+                }
+            }
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void Pause()
+    {
+        pauses.ForEach(n => n.Pause());
+    }
+
+    public void Resume()
+    {
+        pauses.ForEach(n => n.Resume());
+    }
+}
