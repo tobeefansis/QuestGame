a58c48fa55ea7eccb02fb5ee6f4e5fec7ef49f21
tobeefansis <tftsis859@gmail.com>
2020-08-27 20:19:04 +0300

    Update Safe.cs

--- Assets/Scripts/Safe.cs
+++ Assets/Scripts/Safe.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Safe : InteractiveObject
 {
-    public KeyCode open;
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Use") != 0)
+        {
+
+        }
     }
 }
