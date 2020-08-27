77a997e0f07fc888c050b59976ec8abec6710308
sayonarabyee <43315310+sayonarabyee@users.noreply.github.com>
2020-08-27 14:12:32 +0300

    Updated InteractiveObject

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -6,7 +6,4 @@ public class InteractiveObject : MonoBehaviour
     public Canvas canvas;
     public void Show() => canvas.gameObject.SetActive(true);
     public void Hide() => canvas.gameObject.SetActive(false);
-
-    public void Use() {
-    }
 }
---
+++ Assets/Scripts/Safe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Safe : InteractiveObject
+{
+    public KeyCode open;
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
