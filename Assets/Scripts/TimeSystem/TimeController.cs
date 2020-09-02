92e8934a0284ce94453f063436829475c7ac40fb
tobeefansis <tftsis859@gmail.com>
2020-09-02 12:01:44 +0300

    Import & Create Watch

---
+++ Assets/Scripts/TimeSystem/TimeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    private const int SecondsPerDay = 86400;
+    public float time;
+    [Range(0, 10000)] public int timeScale = 1;
+
+    private static TimeController instance;
+
+    public static TimeController Instance { get => instance; private set => instance = value; }
+
+    public string Now => $"{Hour,2}:{Minute,2}:{Second,2}";
+    public int Second => (int)time % 60;
+    public int Minute => ((int)time % 3600) / 60;
+    public int Hour => (int)time / 3600;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+            var _now = DateTime.Now;
+            time = _now.Second + _now.Minute * 60 + _now.Hour * 3600;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    void LateUpdate()
+    {
+        time += Time.deltaTime * timeScale;
+        if (time >= SecondsPerDay)
+        {
+            time -= SecondsPerDay;
+        }
+    }
+}
---
+++ Assets/Scripts/TimeSystem/Watch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Watch : MonoBehaviour
+{
+    [SerializeField] GameObject pointerSeconds;
+    [SerializeField] GameObject pointerMinutes;
+    [SerializeField] GameObject pointerHours;
+
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        var timeCtrl = TimeController.Instance;
+        float rotationSeconds = (360.0f / 60.0f) * timeCtrl.Second;
+        float rotationMinutes = (360.0f / 60.0f) * timeCtrl.Minute;
+        float rotationHours = ((360.0f / 12.0f) * timeCtrl.Hour) + ((360.0f / (60.0f * 12.0f)) * timeCtrl.Minute);
+
+        //-- draw pointers
+        pointerSeconds.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationSeconds);
+        pointerMinutes.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationMinutes);
+        pointerHours.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationHours);
+    }
+}
