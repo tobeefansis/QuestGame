74c56ad2424e34a26f0d3593cfcebafa6ff18a6f
sayonarabyee <43315310+sayonarabyee@users.noreply.github.com>
2020-09-04 16:16:51 +0300

    Revert "Revert "Added safe open\close mechanics""
    This reverts commit 61fe6dc6995c96bb4a4380da30a04a302a6a2fab.

---
+++ Assets/Scripts/PropsControllers/SafeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    public bool isOpen;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpen = false;
+    }
+    public void OpenClose()
+    {
+        isOpen = !isOpen;
+        animator.SetBool("IsOpen", isOpen);
+    }
+
+}
