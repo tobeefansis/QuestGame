3c45ab57fc9a343664b88989f8f5a5e5a380703c
sayonarabyee <43315310+sayonarabyee@users.noreply.github.com>
2020-09-10 18:51:34 +0300

    Added safe keypad functionality

---
+++ Assets/Scripts/KeyPad/Keypad.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+    [Header("Keypad settings")]
+    public string currentPassword = "1234";
+    public string input = "";
+    public Text displayText;
+    public AudioSource correctPassword;
+    public AudioSource incorrectPassword;
+    public Animator openSafe;
+
+    private int buttonClicked = 0;
+    private int numberOfChars;
+
+    public void Start()
+    {
+        numberOfChars = currentPassword.Length;
+    }
+    void CheckLength()
+    {
+        if (input.Length == numberOfChars)
+        {
+            CheckPass();
+        }
+    }
+
+    private void CheckPass()
+    {
+        if (input == currentPassword)
+        {
+            //correctPassword.Play();
+            displayText.gameObject.SetActive(false);
+            var cam = FindObjectOfType<SmartCam>();
+            cam.HideObject();
+            openSafe.SetBool("IsOpen", true);
+        }
+        else
+        {
+            input = "";
+            buttonClicked = 0;
+            displayText.text = input.ToString();
+            incorrectPassword.Play();
+        }
+    }
+
+    public void ValueEntered(string valueEntered)
+    {
+        buttonClicked++;
+        input += valueEntered;
+        displayText.text = input.ToString();
+        CheckLength();
+    }
+
+}
