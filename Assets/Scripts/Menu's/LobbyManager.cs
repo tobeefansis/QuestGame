7d23289b9e16ecb5e138684ddc34b11d6586477d
sayonarabyee <43315310+sayonarabyee@users.noreply.github.com>
2020-09-14 14:44:06 +0300

    Added basic multiplayer connection

---
+++ Assets/Scripts/Menu's/LobbyManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public Text log;
+    void Start()
+    {
+        PhotonNetwork.NickName = "Player" + Random.Range(1,69);
+        Log("Player name set as " + PhotonNetwork.NickName);
+
+        PhotonNetwork.GameVersion = "1.0a";
+        Log("Current game version is " + PhotonNetwork.GameVersion);
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        Log("Connected to master");
+    }
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions {MaxPlayers = 2});
+    }
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnJoinedRoom()
+    {
+        Log("Joined the room");
+        PhotonNetwork.LoadLevel("Game");
+    }
+    private void Log(string message)
+    {
+        Debug.Log(message);
+        log.text += "\n";
+        log.text += message;
+    }
+}
